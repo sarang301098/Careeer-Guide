52cbf9045c770447301a02f52cf74efd799224ea
sarang301098 <61634010+sarang301098@users.noreply.github.com>
2020-06-30 04:35:30 +0530

    Create QuestionBean.java

---
+++ career_guide/src/main/java/com/bean/QuestionBean.java
@@ -0,0 +1,53 @@
+package com.bean;
+
+public class QuestionBean {
+private String questionid,question,optA,optB,optC,field;
+
+public String getQuestionid() {
+	return questionid;
+}
+
+public String getField() {
+	return field;
+}
+
+public void setField(String field) {
+	this.field = field;
+}
+
+public void setQuestionid(String questionid) {
+	this.questionid = questionid;
+}
+public String getQuestion() {
+	return question;
+}
+
+public void setQuestion(String question) {
+	this.question = question;
+}
+
+public String getOptA() {
+	return optA;
+}
+
+public void setOptA(String optA) {
+	this.optA = optA;
+}
+
+public String getOptB() {
+	return optB;
+}
+
+public void setOptB(String optB) {
+	this.optB = optB;
+}
+
+public String getOptC() {
+	return optC;
+}
+
+public void setOptC(String optC) {
+	this.optC = optC;
+}
+
+}
